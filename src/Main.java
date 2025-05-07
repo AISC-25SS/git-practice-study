4bcf0daacf45e36aeb156437fa8d4f6a60eac65c
seungjunGong <jjw5741@naver.com>
2025-05-07 11:37:51 +0900

    docs: 세미나 세팅

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
-import test.GitTest;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
-        GitTest gitTest = new GitTest();
-
-        gitTest.print_message();
+        System.out.println("Hello Git Study!");
 
     }
 }
