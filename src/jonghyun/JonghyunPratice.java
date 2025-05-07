19e9125b00c140c58862e724024a8fba4805a264
user <poreer0597@hs.ac.kr>
2025-05-07 23:19:37 +0900

    feat : add pratice jonghyun greet

---
+++ src/jonghyun/JonghyunPratice.java
@@ -0,0 +1,12 @@
+package jonghyun;
+
+public class JonghyunPratice {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is Jonghyun's Git Practice!");
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("안녕하세요! Git 연습 중입니다!");
+    }
+}
