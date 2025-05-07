3903d5cba69a6fcf7d1d994c31bbd88623cfb4c8
DESKTOP-5FU4EFM\Home <huhjhace@gmail.com>
2025-05-07 22:41:19 +0900

    ### 제목 # 커밋 타입 : 작업 내용

---
+++ src/jihoon/JihoonPractice.java
@@ -0,0 +1,11 @@
+package jihoon;
+
+public class JihoonPractice {
+    public static void greet() {
+        System.out.println("reqeqw");
+    }
+    public static void main(String[] args) {
+
+        greet();
+    }
+}
