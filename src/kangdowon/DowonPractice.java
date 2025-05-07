56a871f13b3c112fa3251576e4ef38b9b9dbf10b
dowon <kd1412@hs.ac.kr>
2025-05-07 22:41:21 +0900

    feet: add practice greet
    - 깃 연습 중

---
+++ src/kangdowon/DowonPractice.java
@@ -0,0 +1,14 @@
+package kangdowon;
+
+public class DowonPractice {
+
+    public static void main(String[] args){
+        System.out.println("Hell0 Git!");
+        greet();
+    }
+
+    public static void greet(){
+        System.out.println(" git 연습 페이지에 오신것을 환영합니다. ");
+    }
+
+}
