48941dbef640dc9bf59b57b4db4307b955a357c3
seungjunGong <jjw5741@naver.com>
2025-05-07 22:41:42 +0900

    feat: add practice greet
    - 깃 연습 중

--- src/seungjun/SeungjunPractice.java
+++ src/seungjun/SeungjunPractice.java
@@ -2,8 +2,13 @@ package seungjun;
 
 public class SeungjunPractice {
 
-    void SeungjunPrint(){
-        System.out.println();
+    public static void main(String[] args){
+        System.out.println("Hello, this is Seungjun's Git Practice!");
+        greet();
+    }
+
+    public static void greet(){
+        System.out.println("안녕하세요! 깃 연습중 입니다!");
     }
 
     // 여기에 본인이 담당한 기능을 구현해 보세요!!
