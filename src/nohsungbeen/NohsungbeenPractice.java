3a0f5b06734b0c1736179fdf5090f9a9dbaaad2f
NohSungBeen <castlekong1019@gmail.com>
2025-05-07 22:41:52 +0900

    feat: 성빈 practice greet 추가
    성빈 practice 와 greet 클래스 생성 및 println 구현

---
+++ src/nohsungbeen/NohsungbeenPractice.java
@@ -0,0 +1,12 @@
+package nohsungbeen;
+
+public class NohsungbeenPractice {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is SungBeen's Git Practice!");
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("안녕하세요! Git 연습 중입니다!");
+    }
+}
