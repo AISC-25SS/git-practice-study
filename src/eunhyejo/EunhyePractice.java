41ee8e402501db3f54bbd1cd2653c583aad9d4b0
eunhy <dodkk98@gmail.com>
2025-05-07 22:41:37 +0900

    feat : 연습 greet 추가함.

---
+++ src/eunhyejo/EunhyePractice.java
@@ -0,0 +1,13 @@
+package eunhyejo;
+
+public class EunhyePractice {
+    public static void main(String[] args) {
+        System.out.println("\nHello, eunhye Git Practice!!");
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("안녕하세요, 은혜는 깃 연습중입니다.");
+
+    }
+}
