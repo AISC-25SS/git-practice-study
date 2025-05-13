d4d3cb213169c3c2a2eb8f7a59b78d0f9ef76de8
Binz <singriet0824@hs.ac.kr>
2025-05-13 23:20:05 +0900

    과제만 남았따

--- src/jongbin/JongbinPractice.java
+++ src/jongbin/JongbinPractice.java
@@ -4,10 +4,10 @@ public class JongbinPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is Binz's Git Practice!");
-        greet();
+        gree();
     }
 
-    public static void greet() {
+    public static void gree() {
         System.out.println("안녕하세요! Git 연습 중입니다!");
     }
 }
