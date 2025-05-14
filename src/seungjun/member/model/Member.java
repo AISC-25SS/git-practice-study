595a2fc9402574c5258b489f8b800cf111f603f6
seungjunGong <jjw5741@naver.com>
2025-05-14 14:05:34 +0900

    feat: member model 추가

---
+++ src/seungjun/member/model/Member.java
@@ -0,0 +1,30 @@
+package seungjun.member.model;
+
+public class Member {
+    private Long memberId;         // 고유 회원 ID
+    private String loginId;
+    private String loginPw;
+    private String name;     // 이름
+    private Grade grade;     // BASIC or VIP
+
+    public Member(Long memberId, String loginId, String loginPw, String name, Grade grade) {
+        this.memberId = memberId;
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // getter 우선 입력만
+    public Long getMemberId() { return memberId; }
+    public String getLoginId() { return loginId; }
+    public String getLoginPw() { return loginPw; }
+    public String getName() { return name; }
+    public Grade getGrade() { return grade; }
+
+
+    @Override
+    public String toString() {
+        return "ID: " + memberId + ", 이름: " + name + " , 등급: '" + grade + "'";
+    }
+}
---
+++ src/seungjun/member/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package seungjun.member.repository;
+
+import seungjun.member.model.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+    private final Map<Long, Member> userInfo = new HashMap<>();
+    private Long memberId = 1L;
+
+    public Member save(Member member){
+        Member newMember = new Member(
+                this.memberId++,
+                member.getLoginId(),
+                member.getLoginPw(),
+                member.getName(),
+                member.getGrade()
+                );
+
+        userInfo.put(newMember.getMemberId(), newMember);
+        return newMember;
+    }
+
+    Member findByLoginId(String loginId) {
+        for(Member member : userInfo.values()) {
+            if(member.getLoginId().equals(loginId)){
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(userInfo.values());
+    }
+}
